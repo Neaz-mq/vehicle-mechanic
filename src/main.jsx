0a3203fc841c3b65677554c4b244f857738a80d0
Neaz-mq <neazmorshed666@gmail.com>
2023-12-16 10:27:12 +0600

    added Home page

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-import {
-  createBrowserRouter,
+import { 
   RouterProvider,
 } from "react-router-dom";
+import router from './Routes/Routes';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
