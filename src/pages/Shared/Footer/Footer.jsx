00c4215e45f9ac020706bcefc5c3df23f72c3d60
Neaz-mq <neazmorshed666@gmail.com>
2023-12-16 11:18:49 +0600

    added header and footer

---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,35 @@
+import logo from '../../../assets/images/logo.png';
+
+const Footer = () => {
+    return (
+        <footer className="footer p-10 bg-slate-900 text-white text-base-content">
+            <aside>
+                
+                <a href="/"><img className='w-24 bg-red-500' src={logo} alt="" /></a>
+                <p>Vehicle Mechanic Ltd.<br />Providing reliable tech since 1999</p>
+            </aside>
+            <nav>
+                <header className="footer-title">Services</header>
+                <a className="link link-hover">Branding</a>
+                <a className="link link-hover">Design</a>
+                <a className="link link-hover">Marketing</a>
+                <a className="link link-hover">Advertisement</a>
+            </nav>
+            <nav>
+                <header className="footer-title">Company</header>
+                <a className="link link-hover">About us</a>
+                <a className="link link-hover">Contact</a>
+                <a className="link link-hover">Jobs</a>
+                <a className="link link-hover">Press kit</a>
+            </nav>
+            <nav>
+                <header className="footer-title">Legal</header>
+                <a className="link link-hover">Terms of use</a>
+                <a className="link link-hover">Privacy policy</a>
+                <a className="link link-hover">Cookie policy</a>
+            </nav>
+        </footer>
+    );
+};
+
+export default Footer;
