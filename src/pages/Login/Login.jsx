bbe37192492184edc6fbf7d3922e8b157c91587b
Neaz-mq <neazmorshed666@gmail.com>
2023-12-19 10:56:19 +0600

    signup page added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,29 @@
 import { Link } from "react-router-dom";
 import img from '../../assets/images/login/login.svg'
+// import { useContext } from "react";
 
 
 const Login = () => {
+
+    // const { signIn } = useContext();
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, email, password)
+        // signIn(email, password)
+        //     .then(result => {
+        //         const user = result.user;
+        //         console.log(user);
+        //     })
+        //     .catch(error => console.log(error));
+    }
+
+
+
+
     return (
 <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
@@ -12,7 +33,7 @@ const Login = () => {
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
                         <h1 className="text-3xl text-center font-bold">Login</h1>
-                        <form >
+                        <form onSubmit={handleLogin}>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
